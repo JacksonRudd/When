ca067bea95f8329207b89472bf7df0b358c04af6
Jackson <jackson.h.rudd@gmail.com>
2024-11-03 13:22:19 -0800

    done

--- when/src/App.tsx
+++ when/src/App.tsx
@@ -9,10 +9,8 @@ function App() {
       <NavBar></NavBar>
       <h1>Timeline</h1>
 
-      <main className="content">
-        <Sidepanel></Sidepanel>
-        <Timeline></Timeline>
-      </main>
+      <Sidepanel></Sidepanel>
+      <Timeline></Timeline>
     </>
   );
 }
